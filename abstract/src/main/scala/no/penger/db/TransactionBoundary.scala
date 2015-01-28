5843e88bc3a5d49270557b639249c525dceb1f55
eirmelan <eirik.meland@finn.no>
2015-01-28 11:03:23 +0100

    Joining transactions

--- abstract/src/main/scala/no/penger/db/TransactionBoundary.scala
+++ abstract/src/main/scala/no/penger/db/TransactionBoundary.scala
@@ -10,5 +10,15 @@ trait TransactionBoundary
   trait Transaction {
     def readOnly[A](f: Tx[RO] => A): A
     def readWrite[A](f: Tx[RW] => A): A
+
+    def joinReadOnly[A](otx: Option[Tx[RO]])(block: Tx[RO] ⇒ A) = {
+      if (otx.isDefined) block(otx.get)
+      else transaction readOnly { tx ⇒ block(tx) }
+    }
+
+    def joinReadWrite[A](otx: Option[Tx[RW]])(block: Tx[RW] ⇒ A) = {
+      if (otx.isDefined) block(otx.get)
+      else transaction readWrite { tx ⇒ block(tx) }
+    }
   }
 }
