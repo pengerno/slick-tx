3a083e164ea0b54a153c762a552451dd1f961160
oyviberg <oyvind.raddum.berg@finn.no>
2015-04-22 14:34:35 +0200

    Bytter navn på `T` i TransactionAware for å unngå warnings for navnekollisjoner i klientene

--- abstract/src/main/scala/no/penger/db/TransactionAware.scala
+++ abstract/src/main/scala/no/penger/db/TransactionAware.scala
@@ -5,8 +5,8 @@ import scala.language.higherKinds
  * This trait is to be implemented by abstract repositories
  */
 trait TransactionAware {
-  sealed trait T
-  trait RO extends T
+  sealed trait TxType
+  trait RO extends TxType
   trait RW extends RO
 
   type Tx[+T]
